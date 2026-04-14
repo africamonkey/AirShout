26196debcf760ddd8b667f21151a8c89adb307e8
africamonkey <marses@vip.qq.com>
2026-04-14 22:02:09 +0800

    feat: add TabView for AirPlay and P2P modes

--- AirShout/AirShoutApp.swift
+++ AirShout/AirShoutApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct AirShoutApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
---
+++ AirShout/Views/MainTabView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("AirPlay", systemImage: "airplayaudio")
+                }
+
+            P2PView()
+                .tabItem {
+                    Label("AirShout", systemImage: "wave.3.right")
+                }
+        }
+    }
+}
